005483b0d587aae0cf32e866be96ea0146b7b8d2
Enrique.Assis <enrique_assis@hotmail.com>
2020-02-28 16:58:13 -0500

    Json request working. Json post fully working.

--- app/src/main/java/com/kuka/kmr_demo/MainActivity.java
+++ app/src/main/java/com/kuka/kmr_demo/MainActivity.java
@@ -1,11 +1,18 @@
 package com.kuka.kmr_demo;
 
+/* Developed by Enrique Assis
+    2020-02-28
+ */
+
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +27,7 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -33,14 +41,186 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
-    private RequestQueue queue;
+    private RequestQueue queue; //for requesting Json
+    private RequestQueue requestQueue; //for sending Json
 
-    private RequestQueue requestQueue;
+    /* Cells 1 to 6 */
+    private ImageButton goto1;
+    private ImageButton goto2;
+    private ImageButton goto3;
+    private ImageButton goto4;
+    private ImageButton goto5;
+    private ImageButton goto6;
+
+    /* Test for KMR position */
+    private Button it_is_at1;
+    private Button it_is_at2;
+    private Button it_is_at3;
+    private Button it_is_at4;
+    private Button it_is_at5;
+    private Button it_is_at6;
+
+    /* KMR icon position animation*/
+    private ImageView kmr1;
+    private ImageView kmr2;
+    private ImageView kmr3;
+    private ImageView kmr4;
+    private ImageView kmr5;
+    private ImageView kmr6;
+
+    /* KMR position animation variables*/
+    private boolean at_node1 = false;
+    private boolean at_node2 = false;
+    private boolean at_node3 = false;
+    private boolean at_node4 = false;
+    private boolean at_node5 = false;
+    private boolean at_node6 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goto1 = (ImageButton) findViewById(R.id.imageButton1);
+        goto2 = (ImageButton) findViewById(R.id.imageButton2);
+        goto3 = (ImageButton) findViewById(R.id.imageButton3);
+        goto4 = (ImageButton) findViewById(R.id.imageButton4);
+        goto5 = (ImageButton) findViewById(R.id.imageButton5);
+        goto6 = (ImageButton) findViewById(R.id.imageButton6);
+
+//        it_is_at1 = (Button) findViewById(R.id.button11);
+//        it_is_at2 = (Button) findViewById(R.id.button12);
+//        it_is_at3 = (Button) findViewById(R.id.button13);
+//        it_is_at4 = (Button) findViewById(R.id.button14);
+//        it_is_at5 = (Button) findViewById(R.id.button15);
+//        it_is_at6 = (Button) findViewById(R.id.button16);
+//
+//        kmr1 = (ImageView) findViewById(R.id.kmr1);
+//        kmr2 = (ImageView) findViewById(R.id.kmr2);
+//        kmr3 = (ImageView) findViewById(R.id.kmr3);
+//        kmr4 = (ImageView) findViewById(R.id.kmr4);
+//        kmr5 = (ImageView) findViewById(R.id.kmr5);
+//        kmr6 = (ImageView) findViewById(R.id.kmr6);
+
+        //handler check for KMR feedback every 250 milliseconds
+
+        final Handler handler = new Handler();
+        final int delay = 250; //milliseconds
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+
+                // check which node is active to show the solid icon at the position.
+                if (at_node1) {
+                    clearIcons();
+                    kmr1.setVisibility(View.VISIBLE);
+                    at_node1 = true;
+                } else if (at_node2) {
+                    clearIcons();
+                    kmr2.setVisibility(View.VISIBLE);
+                    at_node2 = true;
+
+                } else if (at_node3) {
+                    clearIcons();
+                    kmr3.setVisibility(View.VISIBLE);
+                    at_node3 = true;
+
+                } else if (at_node4) {
+                    clearIcons();
+                    kmr4.setVisibility(View.VISIBLE);
+                    at_node4 = true;
+
+                } else if (at_node5) {
+                    clearIcons();
+                    kmr5.setVisibility(View.VISIBLE);
+                    at_node5 = true;
+
+                } else if (at_node6) {
+                    clearIcons();
+                    kmr6.setVisibility(View.VISIBLE);
+                    at_node6 = true;
+
+                }
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+
+        /* Listener waits a command to move the KMR. After the command, the first
+        action is to verify the current position in order to run the proper animation */
+        /*It has to be added here the command to send the robot to the node*/
+
+        goto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_1\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
+
+        goto2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_2\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
+
+        goto3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_3\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
+
+        goto4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_4\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
+
+        goto5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_5\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
+
+        goto6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPosition();
+
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_6\"\n" +
+                        "}";
+                Submit(data);
+            }
+        });
 
 
         /* *** Getting Json *** */
@@ -52,18 +232,18 @@ public class MainActivity extends AppCompatActivity {
         buttonParse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                jsonParse();
+                jsonRequest();
             }
         });
 
         /* *** Sending Json *** */
 
-
         Button submitButton = (Button) findViewById(R.id.button_parse2);
-
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String data = "[{}]";
+                String data = "{\n" +
+                        "\"jobName\":\"MOVE_TO_CELL_1\"\n" +
+                        "}";
                 Submit(data);
 
             }
@@ -71,10 +251,178 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /* ***********END OF ONCREATE ************ */
 
+    // This method clears all animations, but the one where the KMR currently is.
+    public void checkPosition(){
+//        if (at_node1){
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr1.startAnimation(animation);
+//
+//            kmr2.clearAnimation();
+//            kmr3.clearAnimation();
+//            kmr4.clearAnimation();
+//            kmr5.clearAnimation();
+//            kmr6.clearAnimation();
+//
+//            kmr2.setVisibility(View.INVISIBLE);
+//            kmr3.setVisibility(View.INVISIBLE);
+//            kmr4.setVisibility(View.INVISIBLE);
+//            kmr5.setVisibility(View.INVISIBLE);
+//            kmr6.setVisibility(View.INVISIBLE);
+//
+//            at_node1 = false;
+//        }
+//        else if (at_node2){
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr2.startAnimation(animation);
+//
+//            kmr1.clearAnimation();
+//            kmr3.clearAnimation();
+//            kmr4.clearAnimation();
+//            kmr5.clearAnimation();
+//            kmr6.clearAnimation();
+//
+//            kmr1.setVisibility(View.INVISIBLE);
+//            kmr3.setVisibility(View.INVISIBLE);
+//            kmr4.setVisibility(View.INVISIBLE);
+//            kmr5.setVisibility(View.INVISIBLE);
+//            kmr6.setVisibility(View.INVISIBLE);
+//
+//            at_node2 = false;
+//
+//        }
+//        else if (at_node3) {
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr3.startAnimation(animation);
+//
+//            kmr1.clearAnimation();
+//            kmr2.clearAnimation();
+//            kmr4.clearAnimation();
+//            kmr5.clearAnimation();
+//            kmr6.clearAnimation();
+//
+//            kmr1.setVisibility(View.INVISIBLE);
+//            kmr2.setVisibility(View.INVISIBLE);
+//            kmr4.setVisibility(View.INVISIBLE);
+//            kmr5.setVisibility(View.INVISIBLE);
+//            kmr6.setVisibility(View.INVISIBLE);
+//
+//            at_node3 = false;
+//
+//        }
+//        else if (at_node4) {
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr4.startAnimation(animation);
+//
+//            kmr1.clearAnimation();
+//            kmr2.clearAnimation();
+//            kmr3.clearAnimation();
+//            kmr5.clearAnimation();
+//            kmr6.clearAnimation();
+//
+//            kmr1.setVisibility(View.INVISIBLE);
+//            kmr2.setVisibility(View.INVISIBLE);
+//            kmr3.setVisibility(View.INVISIBLE);
+//            kmr5.setVisibility(View.INVISIBLE);
+//            kmr6.setVisibility(View.INVISIBLE);
+//
+//            at_node4 = false;
+//
+//        }
+//        else if (at_node5) {
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr5.startAnimation(animation);
+//
+//            kmr1.clearAnimation();
+//            kmr2.clearAnimation();
+//            kmr3.clearAnimation();
+//            kmr4.clearAnimation();
+//            kmr6.clearAnimation();
+//
+//            kmr1.setVisibility(View.INVISIBLE);
+//            kmr2.setVisibility(View.INVISIBLE);
+//            kmr3.setVisibility(View.INVISIBLE);
+//            kmr4.setVisibility(View.INVISIBLE);
+//            kmr6.setVisibility(View.INVISIBLE);
+//
+//            at_node5 = false;
+//
+//        }
+//        else if (at_node6) {
+//            final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+//            kmr6.startAnimation(animation);
+//
+//            kmr1.clearAnimation();
+//            kmr2.clearAnimation();
+//            kmr3.clearAnimation();
+//            kmr4.clearAnimation();
+//            kmr5.clearAnimation();
+//
+//            kmr1.setVisibility(View.INVISIBLE);
+//            kmr2.setVisibility(View.INVISIBLE);
+//            kmr3.setVisibility(View.INVISIBLE);
+//            kmr4.setVisibility(View.INVISIBLE);
+//            kmr5.setVisibility(View.INVISIBLE);
+//
+//            at_node6 = false;
+//
+//        }
+    }
+
+
+    public void clearIcons() {
+
+        at_node1 = false;
+        at_node2 = false;
+        at_node3 = false;
+        at_node4 = false;
+        at_node5 = false;
+        at_node6 = false;
+
+        kmr1.clearAnimation();
+        kmr2.clearAnimation();
+        kmr3.clearAnimation();
+        kmr4.clearAnimation();
+        kmr5.clearAnimation();
+        kmr6.clearAnimation();
+
+        kmr1.setVisibility(View.INVISIBLE);
+        kmr2.setVisibility(View.INVISIBLE);
+        kmr3.setVisibility(View.INVISIBLE);
+        kmr4.setVisibility(View.INVISIBLE);
+        kmr5.setVisibility(View.INVISIBLE);
+        kmr6.setVisibility(View.INVISIBLE);
+    }
+
+/*
+    public void clearIcons() {
+
+        at_node1 = false;
+        at_node2 = false;
+        at_node3 = false;
+        at_node4 = false;
+        at_node5 = false;
+        at_node6 = false;
+
+        kmr1.clearAnimation();
+        kmr2.clearAnimation();
+        kmr3.clearAnimation();
+        kmr4.clearAnimation();
+        kmr5.clearAnimation();
+        kmr6.clearAnimation();
+
+        kmr1.setVisibility(View.INVISIBLE);
+        kmr2.setVisibility(View.INVISIBLE);
+        kmr3.setVisibility(View.INVISIBLE);
+        kmr4.setVisibility(View.INVISIBLE);
+        kmr5.setVisibility(View.INVISIBLE);
+        kmr6.setVisibility(View.INVISIBLE);
+    }
+*/
     /* Class to request Json */
 
-    private void jsonParse() {
+    private void jsonRequest() {
 
         String url = "http://10.216.70.19:8080/restServices/webapi/services/getAGVStatusList";
         StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
@@ -101,7 +449,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             // Create JSOn Object
             JSONArray jsonArray = new JSONArray(response);
-            for (int i = 0; i <jsonArray.length() ; i++) {
+            for (int i = 0; i < jsonArray.length(); i++) {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
                 textView.setText(jsonObject.getString("batteryLevel"));
 
@@ -111,52 +459,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-/* Post Json */
-private void Submit(String data)
-{
-    final String savedata= data;
-    String URL="http://10.216.70.19:8080/restServices/webapi/services/createNewJob";
+    /* Post Json */
+    private void Submit(String data) {
+        final String savedata = data;
+        String URL1 = "http://10.216.70.19:8080/restServices/webapi/services/createNewJob";
 
-    requestQueue = Volley.newRequestQueue(getApplicationContext());
-    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-        @Override
-        public void onResponse(String response) {
-            try {
-                JSONObject objres=new JSONObject(response);
-                Toast.makeText(getApplicationContext(),objres.toString(),Toast.LENGTH_LONG).show();
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL1, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject objres = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), objres.toString(), Toast.LENGTH_LONG).show();
 
 
-            } catch (JSONException e) {
-                Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), "Server Error", Toast.LENGTH_LONG).show();
 
+                }
+                //Log.i("VOLLEY", response);
             }
-            //Log.i("VOLLEY", response);
-        }
-    }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
 
-            //Log.v("VOLLEY", error.toString());
-        }
-    }) {
-        @Override
-        public String getBodyContentType() {
-            return "application/json; charset=utf-8";
-        }
-
-        @Override
-        public byte[] getBody() throws AuthFailureError {
-            try {
-                return savedata == null ? null : savedata.getBytes("utf-8");
-            } catch (UnsupportedEncodingException uee) {
-                //Log.v("Unsupported Encoding while trying to get the bytes", data);
-                return null;
+                //Log.v("VOLLEY", error.toString());
             }
-        }
+        }) {
 
-    };
-    requestQueue.add(stringRequest);
-}
+            // post data to server
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return savedata == null ? null : savedata.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    //Log.v("Unsupported Encoding while trying to get the bytes", data);
+                    return null;
+                }
+            }
+
+        };
+        requestQueue.add(stringRequest);
+    }
 }
